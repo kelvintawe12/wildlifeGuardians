687c5dca1bbd3528c0198f893c1ab8b651fb426b
kelvintawe12 <k.tawe@alustudent.com>
2025-07-22 13:52:06 +0200

    refactor: simplify Layout component structure and remove background overlay

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,22 +5,23 @@ import Footer from './Footer';
 
 const Layout: React.FC = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50">
-      <Navbar />
-      
-      <main className="flex-1 relative">
-        <div className="absolute inset-0 opacity-20">
-          <div className="h-full w-full bg-repeat bg-center" style={{
-            backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Cg fill-opacity='0.03'%3E%3Cpolygon fill='%23000' points='50 0 60 40 100 50 60 60 50 100 40 60 0 50 40 40'/%3E%3C/g%3E%3C/svg%3E")`
-          }}></div>
-        </div>
-        
-        <div className="relative z-10">
+    <div className="min-h-screen flex flex-col">
+      {/* Navbar - completely flush */}
+      <header className="w-full">
+        <Navbar />
+      </header>
+
+      {/* Main content with minimal padding */}
+      <main className="flex-1 w-full">
+        <div className="w-full mx-auto max-w-[100vw]">
           <Outlet />
         </div>
       </main>
-      
-      <Footer />
+
+      {/* Footer - completely flush */}
+      <footer className="w-full">
+        <Footer />
+      </footer>
     </div>
   );
 };
